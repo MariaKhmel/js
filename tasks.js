618a462aba37d2b9065c84c23bf53b3de825f9f5
Maria <mariakhmel8@gmail.com>
2025-07-11 23:41:59 +0300

    task3

--- tasks.js
+++ tasks.js
@@ -19,3 +19,19 @@ function getShippingMessage(country, price, deliveryFee) {
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
 console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+
+///task3
+
+function getElementWidth(content, padding, border) {
+  const contentValue = parseFloat(content);
+  const paddingValue = parseFloat(padding);
+  const borderValue = parseFloat(border);
+
+  return contentValue + paddingValue * 2 + borderValue * 2;
+
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
