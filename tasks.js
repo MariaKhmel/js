89f330583463c148b85c7644d1d561ae0bc84c88
Maria <mariakhmel8@gmail.com>
2025-07-13 23:33:53 +0300

    task1

--- tasks.js
+++ tasks.js
@@ -1,40 +1,40 @@
-///task1
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = pricePerDroid * quantity;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
+// ///task1
+// function makeTransaction(quantity, pricePerDroid) {
+//   const totalPrice = pricePerDroid * quantity;
+//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-
-
-///task 2
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
-
-}
-
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
 
 
-///task3
+// ///task 2
+// function getShippingMessage(country, price, deliveryFee) {
+//   const totalPrice = price + deliveryFee;
+//   return `Shipping to ${country} will cost ${totalPrice} credits`;
 
-function getElementWidth(content, padding, border) {
-  const contentValue = parseFloat(content);
-  const paddingValue = parseFloat(padding);
-  const borderValue = parseFloat(border);
+// }
 
-  return contentValue + paddingValue * 2 + borderValue * 2;
+// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
 
-}
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// ///task3
+
+// function getElementWidth(content, padding, border) {
+//   const contentValue = parseFloat(content);
+//   const paddingValue = parseFloat(padding);
+//   const borderValue = parseFloat(border);
+
+//   return contentValue + paddingValue * 2 + borderValue * 2;
+
+// }
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
 
 let a = 5;
 let b = 10;
@@ -46,3 +46,23 @@ a += 2;
 b -= 4;
 c *= 3;
 d /= 10;
+
+//module2
+///task1
+const makeTransaction = function (quantity, pricePerDroid, customerCredits) {
+  const orderSum = pricePerDroid * quantity;
+
+  if (customerCredits < orderSum) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${orderSum} credits!`
+  }
+
+
+}
+
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
