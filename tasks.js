d1a42028f429bd3988f18b4f0c6340a5436318db
Maria <mariakhmel8@gmail.com>
2025-07-16 21:30:19 +0300

    task3

--- tasks.js
+++ tasks.js
@@ -84,3 +84,21 @@ console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+const checkForSpam = (message) => {
+  const messageArr = message.split(" ");
+  return messageArr.forEach(word => {
+    if (word.toLowerCase() === "spam" || word.toLowercase() === "sale") {
+      return true;
+    }
+  })
+
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
