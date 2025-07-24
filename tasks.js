273ce7aebe0923e36404601d7729a6fd1b04423e
Maria <mariakhmel8@gmail.com>
2025-07-24 23:48:46 +0300

    fx

--- tasks.js
+++ tasks.js
@@ -1,187 +1,214 @@
+// // ///task1
+// // function makeTransaction(quantity, pricePerDroid) {
+// //   const totalPrice = pricePerDroid * quantity;
+// //   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// // }
+
+// // console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// // console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// // console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+
+// // ///task 2
+// // function getShippingMessage(country, price, deliveryFee) {
+// //   const totalPrice = price + deliveryFee;
+// //   return `Shipping to ${country} will cost ${totalPrice} credits`;
+
+// // }
+
+// // console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+// // console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+// // console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+
+// // ///task3
+
+// // function getElementWidth(content, padding, border) {
+// //   const contentValue = parseFloat(content);
+// //   const paddingValue = parseFloat(padding);
+// //   const borderValue = parseFloat(border);
+
+// //   return contentValue + paddingValue * 2 + borderValue * 2;
+
+// // }
+
+// // console.log(getElementWidth("50px", "8px", "4px")); // 74
+// // console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// // console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+
+// // Change code below this line
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+// //module2
 // ///task1
-// function makeTransaction(quantity, pricePerDroid) {
-//   const totalPrice = pricePerDroid * quantity;
-//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-// }
+// const makeTransaction = function (quantity, pricePerDroid, customerCredits) {
+//   const orderSum = pricePerDroid * quantity;
 
-// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+//   if (customerCredits < orderSum) {
+//     return "Insufficient funds!";
+//   } else {
+//     return `You ordered ${quantity} droids worth ${orderSum} credits!`
+//   }
 
-
-// ///task 2
-// function getShippingMessage(country, price, deliveryFee) {
-//   const totalPrice = price + deliveryFee;
-//   return `Shipping to ${country} will cost ${totalPrice} credits`;
 
 // }
 
-// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+// console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+
+// ///task2
+// function formatMessage(message, maxLength) {
+//   if (message.length <= maxLength) {
+//     return message;
+//   } else if (message.length > maxLength) {
+//     return message.slice(0, maxLength) + "...";
+//   }
+
+// }
+
+// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+// //task3
+// const checkForSpam = (message) => {
+//   const lowerCaseMessage = message.toLowerCase();
+//   return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
+// }
+
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+// console.log(checkForSpam("Trust me, this is not a spam message")); // true
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+// //task4
+// function getShippingCost(country) {
+//   switch (country) {
+//     case "China":
+//       return `Shipping to China will cost 100 credits`;
+
+//     case "Chile":
+//       return `Shipping to Chile will cost 250 credits`;
+
+//     case "Australia":
+//       return `Shipping to Australia will cost 170 credits`;
+
+//     case "China":
+//       return `Shipping to Jamaica will cost 120 credits`;
+//     default:
+//       return "Sorry, there is no delivery to your country";
+//   }
+// }
+
+// console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+// console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+// console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+// console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+// console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+
+
+// ///module3
+// //task2
+
+// function slugify(title) {
+//   return title.split(" ").join("-").toLowerCase();
+// }
+
+// console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+// console.log(slugify("English for developer")); // "english-for-developer"
+// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+
+// ///task2
+
+// const makeArray = (firstArray, secondArray, maxLength) => {
+//   const newArray = [...firstArray, ...secondArray];
+
+//   if (newArray.length > maxLength) {
+//     return newArray.slice(0, maxLength);
+//   }
+//   return newArray;
+// }
+
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
 
 
 // ///task3
 
-// function getElementWidth(content, padding, border) {
-//   const contentValue = parseFloat(content);
-//   const paddingValue = parseFloat(padding);
-//   const borderValue = parseFloat(border);
+// //solution 1
+// // const filterArray = (numbers, value) => {
+// //   const newArr = [];
 
-//   return contentValue + paddingValue * 2 + borderValue * 2;
+// //   for (let i = 0; i < numbers.length; i++) {
+// //     if (numbers[i] > value) {
+// //       newArr.push(numbers[i]);
+// //     }
+// //   }
 
-// }
+// //   return newArr;
+// // }
 
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-let a = 5;
-let b = 10;
-let c = 15;
-let d = 20;
-
-// Change code below this line
-a += 2;
-b -= 4;
-c *= 3;
-d /= 10;
-
-//module2
-///task1
-const makeTransaction = function (quantity, pricePerDroid, customerCredits) {
-  const orderSum = pricePerDroid * quantity;
-
-  if (customerCredits < orderSum) {
-    return "Insufficient funds!";
-  } else {
-    return `You ordered ${quantity} droids worth ${orderSum} credits!`
-  }
+// //solution2
 
 
-}
-
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
-
-
-///task2
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  } else if (message.length > maxLength) {
-    return message.slice(0, maxLength) + "...";
-  }
-
-}
-
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
-//task3
-const checkForSpam = (message) => {
-  const lowerCaseMessage = message.toLowerCase();
-  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
-}
-
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-
-//task4
-function getShippingCost(country) {
-  switch (country) {
-    case "China":
-      return `Shipping to China will cost 100 credits`;
-
-    case "Chile":
-      return `Shipping to Chile will cost 250 credits`;
-
-    case "Australia":
-      return `Shipping to Australia will cost 170 credits`;
-
-    case "China":
-      return `Shipping to Jamaica will cost 120 credits`;
-    default:
-      return "Sorry, there is no delivery to your country";
-  }
-}
-
-console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
-
-
-///module3
-//task2
-
-function slugify(title) {
-  return title.split(" ").join("-").toLowerCase();
-}
-
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-
-
-///task2
-
-const makeArray = (firstArray, secondArray, maxLength) => {
-  const newArray = [...firstArray, ...secondArray];
-
-  if (newArray.length > maxLength) {
-    return newArray.slice(0, maxLength);
-  }
-  return newArray;
-}
-
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
-
-
-///task3
-
-//solution 1
 // const filterArray = (numbers, value) => {
-//   const newArr = [];
-
-//   for (let i = 0; i < numbers.length; i++) {
-//     if (numbers[i] > value) {
-//       newArr.push(numbers[i]);
-//     }
-//   }
-
-//   return newArr;
+//   return numbers.filter((el) => el > value);
 // }
 
-//solution2
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
 
+///module 4
 
-const filterArray = (numbers, value) => {
-  return numbers.filter((el) => el > value);
+//task1
+
+const isEnoughCapacity = function (products, containerSize) {
+  for (const product in products) {
+    console.log(product)
+  }
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
