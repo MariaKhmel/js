bdff02a6357a81db75a1db6cdbb93cfe5e02b532
Maria <mariakhmel8@gmail.com>
2025-07-25 18:13:33 +0300

    task2

--- tasks.js
+++ tasks.js
@@ -190,27 +190,71 @@
 
 //task1
 
-const isEnoughCapacity = function (products, containerSize) {
-  let sum = 0;
-  for (const product in products) {
-    sum += products[product];
+// const isEnoughCapacity = function (products, containerSize) {
+//   let sum = 0;
+//   for (const product in products) {
+//     sum += products[product];
+//   }
+//   return sum >= containerSize ? true : false;
+// }
+
+
+// console.log(
+//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+// ); // true
+
+// console.log(
+//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+// ); // false
+
+// console.log(
+//   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+// ); // true
+
+// console.log(
+//   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+// ); // false
+
+//task2
+
+function calcAverageCalories(days) {
+  let averageCalories = 0;
+  let weeklyCalories = 0;
+  if (days.length > 0) {
+    for (const day in days) {
+      console.log(day)
+      weeklyCalories += day.calories;
+    }
+    averageCalories = weeklyCalories / days.length;
   }
-  return sum >= containerSize ? true : false;
+
+  return averageCalories;
 }
 
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
 
 console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
 
 console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); // false
-
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-); // true
-
-console.log(
-  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-); // false
+  calcAverageCalories([])
+); // 0
