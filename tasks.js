fb50393eb8dae17003eaf2371bc9ad516580475c
Maria <mariakhmel8@gmail.com>
2025-07-27 19:15:39 +0300

    task 3

--- tasks.js
+++ tasks.js
@@ -217,43 +217,68 @@
 
 //task2
 
-function calcAverageCalories(days) {
-  let averageCalories = 0;
-  let weeklyCalories = 0;
-  if (days.length > 0) {
-    for (const day of days) {
-      weeklyCalories += day.calories;
-    }
-    averageCalories = weeklyCalories / days.length;
-  }
+// function calcAverageCalories(days) {
+//   let averageCalories = 0;
+//   let weeklyCalories = 0;
+//   if (days.length > 0) {
+//     for (const day of days) {
+//       weeklyCalories += day.calories;
+//     }
+//     averageCalories = weeklyCalories / days.length;
+//   }
 
-  return averageCalories;
-}
+//   return averageCalories;
+// }
 
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 3010 },
-    { day: "tuesday", calories: 3200 },
-    { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
-  ])
-); // 3180
+// console.log(
+//   calcAverageCalories([
+//     { day: "monday", calories: 3010 },
+//     { day: "tuesday", calories: 3200 },
+//     { day: "wednesday", calories: 3120 },
+//     { day: "thursday", calories: 2900 },
+//     { day: "friday", calories: 3450 },
+//     { day: "saturday", calories: 3280 },
+//     { day: "sunday", calories: 3300 }
+//   ])
+// ); // 3180
 
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },
-    { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
-  ])
-); // 2270
+// console.log(
+//   calcAverageCalories([
+//     { day: "monday", calories: 2040 },
+//     { day: "tuesday", calories: 2270 },
+//     { day: "wednesday", calories: 2420 },
+//     { day: "thursday", calories: 1900 },
+//     { day: "friday", calories: 2370 },
+//     { day: "saturday", calories: 2280 },
+//     { day: "sunday", calories: 2610 }
+//   ])
+// ); // 2270
 
-console.log(
-  calcAverageCalories([])
-); // 0
+// console.log(
+//   calcAverageCalories([])
+// ); // 0
+
+//task3
+
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`
+  },
+};
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
