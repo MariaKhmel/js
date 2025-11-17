b3d811fdc7161066f795e1875fc819cca0f7b037
Maria <mariakhmel8@gmail.com>
2025-11-17 04:00:23 +0200

    nested

--- tasks.js
+++ tasks.js
@@ -525,4 +525,10 @@
 //   }
 // ]
 
-gs.info("hi")
+// var id = 1;
+// for (var team = 1; team <= 5; team++) {
+//   for (var person = 1; person <= 4; person++) {
+//     gs.info("team=" + team + "person=" + person + "id =" + id);
+//     ++id;
+//   }
+// }
